37703d7e66d94346fe3816d2ac6300b5cfb47fd1
FarghlyMal <133105828+FarghlyMal@users.noreply.github.com>
2023-05-25 23:09:17 +0300

    Update and rename ICEDID_CONfig.py to ICEDID_Config.py

--- ICEDID_Config.py
+++ ICEDID_Config.py
@@ -2,7 +2,7 @@ from arc4 import ARC4
 import binascii
 import pefile
 
-def config_extract(filename):      		#this function parse pe file and extract the data from '.data' section
+def config_extract(filename):      	        	#this function parse pe file and extract the data from '.data' section
     pe = pefile.PE(filename)
     for section in pe.sections:
        if b".data" in section.Name :
@@ -10,17 +10,17 @@ def config_extract(filename):      		#this function parse pe file and extract th
            
         
         
-def rc4_decrypt(key, data):   			# RC4 Decryption
+def rc4_decrypt(key, data):   			        # RC4 Decryption
     cipher = ARC4(key)
     decrypted = cipher.decrypt(data)
     return decrypted 
 
 def main():
    # print(input("Enter File Name"))
-    data =  config_extract('File name path ') 	# like C:\\Users\\ICEDID.bin
+    data =  config_extract('File name path ') 	    # like C:\\Users\\ICEDID.bin
 
 #   data = binascii.unhexlify(data)
-    key = data[:8] 				 #key is the first 8 bytes of the Blob
+    key = data[:8] 				                     #key is the first 8 bytes of the Blob
     data = data[8:592]    	
     decrypted_data = rc4_decrypt(key, data)
    # DA = decrypted_data.replace(b'\x00', b'').split(b'\x00')
